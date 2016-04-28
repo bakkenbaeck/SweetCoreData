eff7b3d996d52abbe8f5105574460f085053983c
Elvis Nuñez <elvisnunez@me.com>
2016-04-28 22:37:42 +0200

    Add tests

--- Tests/NSManagedObjectContext+SweetnessTests.swift
+++ Tests/NSManagedObjectContext+SweetnessTests.swift
@@ -3,11 +3,23 @@ import XCTest
 import DATAStack
 
 class NSManagedObjectContext_SweetnessTests: XCTestCase {
-    /*
     func testDropEntity() {
         let stack = DATAStack(modelName: "Tests", bundle: NSBundle(forClass: NSManagedObjectContext_SweetnessTests.self), storeType: .SQLite)
         try! stack.mainContext.dropEntity("User")
         stack.drop()
     }
-    */
+
+    func testCountEntity() {
+        let stack = DATAStack(modelName: "Tests", bundle: NSBundle(forClass: NSManagedObjectContext_SweetnessTests.self), storeType: .SQLite)
+        let count = stack.mainContext.countEntity("User")
+        print(count)
+        stack.drop()
+    }
+
+    func testFetchEntity() {
+        let stack = DATAStack(modelName: "Tests", bundle: NSBundle(forClass: NSManagedObjectContext_SweetnessTests.self), storeType: .SQLite)
+        let users = try! stack.mainContext.fetchEntity("User")
+        print(users)
+        stack.drop()
+    }
 }
